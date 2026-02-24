29aa547731f74ab5dc563f5288bb453dc8a9b310
David P. Discher <dpd@dpdtech.com>
2026-02-23 23:35:39 -0800

    Implement ingest rate adjustments

--- run-refvideo-test.sh
+++ run-refvideo-test.sh
@@ -5,16 +5,16 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 MODEL="artifacts/models/communitycats-prod-20260217-213759/exports/best.mlpackage"
 #MODEL="yolov8s.pt"
 
-video_dir="/Users/dpd/Movies/tapo-cat1/*.mp4"
 video_dir="/Volumes/camera/communitycats/referenceVideos/*.mp4"
+video_dir="/Users/dpd/Movies/tapo-cat1/*.mp4"
 
 for video in  $video_dir; do
     echo "$video"
     ./cds detect --uri $video \
     --model-path $MODEL \
     --imgsz 416 --nms 0.6 \
-    --confidence 0.40 
-#    --benchmark \
+    --confidence 0.60  \
+    --benchmark
 #     --confidence-min 0.60 \
 #     --export-frames \
 #     --export-frames-dir artifacts/exports/active-learning \
