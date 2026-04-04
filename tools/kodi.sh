5ba597ea1e67bee6e025da1e267cd551723f893f
David P. Discher <dpd@dpdtech.com>
2026-04-03 22:20:45 -0700

    addin matrix paging

--- tools/kodi.sh
+++ tools/kodi.sh
@@ -7,11 +7,21 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 case $1 in
     cat)
         FILE="${SCRIPT_DIR}/kodi-cat.json"
+        MSG="cat Detected ! ($CDS_CONFIDENCE)"
     ;;
     raccoon)
     	FILE="${SCRIPT_DIR}/kodi-raccoon.json"
+        MSG="!! Raccoon Detected !! ($CDS_CONFIDENCE)"
+    ;;
+    *)
+        exit;
     ;;
 esac
 
 curl -X POST -H "Content-Type: application/json" \
     -d @${FILE} http://kodi:kodi@kodi-main:8080/jsonrpc
+
+if [ -x "${SCRIPT_DIR}/.env/bin/matrix-commander" ]; then
+    matrix-commander -m "$MSG" --user @daviddpd:matrix.org
+fi
+
